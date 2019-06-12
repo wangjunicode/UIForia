7a5d1a0a5ef4dad0ffbc762de88908b2f04a1978
Christian Riedel <christian@klang-games.com>
2019-06-12 17:58:38 +0200

    Reorder style state prio

--- Packages/UIForia/Src/Rendering/UIStyleSet/StyleState.cs
+++ Packages/UIForia/Src/Rendering/UIStyleSet/StyleState.cs
@@ -7,8 +7,8 @@ namespace UIForia.Rendering {
 
         // todo -- reorganize by priority since this is a sort key
         Normal = 1 << 0,
-        Active = 1 << 1,
-        Hover = 1 << 2,
+        Hover = 1 << 1,
+        Active = 1 << 2,
         Focused = 1 << 3,
 
     }

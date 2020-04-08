a9cfaae0151715375376a0fe309f13cae13d6f12
weichx <weichx@users.noreply.github.com>
2020-04-08 14:30:35 +0200

    added shadow bug demo

---
+++ Assets/GraphicsBug/GraphicsBug_Shadow.cs
@@ -0,0 +1,9 @@
+using UIForia.Attributes;
+using UIForia.Elements;
+
+namespace BrokenUI {
+
+    [Template("GraphicsBug/GraphicsBug_Shadow.xml")]
+    public class GraphicsBug_Shadow : UIElement { }
+
+}

b0307be2575f9681b58871a9def440a83fcbc5e5
Roman Hwang <roman@klang-games.com>
2020-10-19 12:41:43 +0200

    Added InputField

---
+++ Assets/SeedLib/InputField/InputField.cs
@@ -0,0 +1,8 @@
+using UIForia.Attributes;
+using UIForia.Elements;
+
+namespace SeedLib {
+    [Template("SeedLib/InputField/InputField.xml")]
+    public class InputField : InputElement<string> {
+    }
+}

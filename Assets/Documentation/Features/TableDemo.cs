cfe3c8808246fc4730b6bb2c3633d1ce3940e985
Roman Hwang <roman@klang-games.com>
2021-01-05 14:30:54 +0100

    table wip

---
+++ Assets/Documentation/Features/TableDemo.cs
@@ -0,0 +1,8 @@
+using UIForia.Attributes;
+using UIForia.Elements;
+
+namespace Documentation.Features {
+    [Template("Documentation/Features/TableDemo.xml")]
+    public class TableDemo : UIElement {
+    }
+}

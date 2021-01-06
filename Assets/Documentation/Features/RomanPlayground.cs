4b927d0d60081564428619bd21c9e3d07cab9409
Roman Hwang <roman@klang-games.com>
2021-01-06 11:43:39 +0100

    Removed RomanPlayground

--- Assets/Documentation/Features/RomanPlayground.cs
+++
@@ -1,8 +0,0 @@
-using UIForia.Attributes;
-using UIForia.Elements;
-
-namespace Documentation.Features {
-    [Template("Documentation/Features/RomanPlayground.xml")]
-    public class RomanPlayground : UIElement {
-    }
-}
